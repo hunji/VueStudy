8ce85d79003653b7c03cf0bad2d0a4f204b1a918
hunji <guo194194@gmail.com>
2018-01-02 23:59:11 +0800

    把本地代码提交到git First

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ComA number="666" @my-event="getMyEvent">
+      <a>這是一個子組件</a>
+    </ComA>
+    <p is="com-a"></p>
+    <p :is="comToRender"></p>
+    <com-a></com-a>
+    <div>
+      {{myVal}}
+    </div>
+    <input type="text" v-model.number="myVal"/>
+  </div>
+</template>
+
+<script>
+  import ComA from './components/a'
+  export default {
+    components: { ComA },
+    data () {
+      return {
+        myVal: '父组件内容',
+        comToRender: 'ComA'
+      }
+    },
+    methods: {
+      getMyEvent (hello) {
+        console.log('触发了父组件事件' + hello)
+      }
+    }
+
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/a.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  {{ number }}
+  {{ hello }}
+  <button @click="emitMyEvent">emit</button>
+  <slot>
+    no slot
+  </slot>
+</div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{
+    props: ['number'],
+    data () {
+      return {
+        hello: 'i am componentA'
+      }
+    },
+    methods: {
+      emitMyEvent () {
+        this.$emit('my-event', this.hello)
+      }
+    }
+  }
+</script>
