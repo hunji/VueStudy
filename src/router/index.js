9c4e8fa33bf4b544a4982a2561bdb59e789ae4b4
hunji <guo194194@gmail.com>
2018-01-04 16:14:55 +0800

    项目基本结构，layout构建

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import IndexPage from '@/pages/index'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld
+      component: IndexPage
     }
   ]
 })
