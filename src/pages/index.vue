a3dda1845f0ffb25d385f60107e2ed312f56e993
hunji <guo194194@gmail.com>
2020-02-16 18:55:14 +0800

    添加merge测试

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="index-wrap">
     <div class="index-left">
       <div class="index-left-block">
-        <h2>全部产品</h2>
+        <h2>全部产品testName</h2>
         <template v-for="product in productList">
           <h3>{{ product.title }}</h3>
           <ul>
